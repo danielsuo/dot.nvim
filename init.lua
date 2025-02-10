b81e49d2c0734ff1433564090f77b8ba69b7acd5
Daniel Suo <danielsuo@gmail.com>
2025-02-10 19:27:06 +0000

    Add nvgoog

--- init.lua
+++ init.lua
@@ -948,6 +948,10 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  {
+    url = 'sso://user/fentanes/nvgoog',
+    import = 'nvgoog.default',
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
